989b482836ab2f5e803c46d2da76d5b1eb48d814
Jacob Martinson <jacobm3@gmail.com>
2019-06-07 12:45:03 -0500

    Cleanup pki scripts.

--- pki/01-gen-certs.sh
+++ pki/01-gen-certs.sh
@@ -4,8 +4,9 @@ vault write pki/issue/hashicorp-test-dot-com \
     common_name=db1.hashicorp-test.com \
     format=pem_bundle > db1.hashicorp-test.com
 
-for x in `seq 1 9`; do 
+for x in `seq 1 5`; do 
     vault write pki/issue/hashicorp-test-dot-com \
          common_name=app${x}.hashicorp-test.com \
          format=pem_bundle > app${x}.hashicorp-test.com
 done
+
